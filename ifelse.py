44057488e3f2b2acf0c4329b359aa0bab39cdc86
sbasmanth <104232714+sbasmanth@users.noreply.github.com>
2022-04-23 04:39:30 +0700

    Add files via upload
    https://youtu.be/Hqndpzj0ZFg

---
+++ ifelse.py
@@ -0,0 +1,16 @@
+#cek apakah sebuah angka positif atau negatif
+#kemudian tampilkan di layar
+
+from ast import Num
+
+
+Num = 3
+
+#masukan variasi lainnya juga
+Num = -5
+Num = 0
+
+if Num >= 0:
+    print("positif atau nol")
+else:
+    print("negatif atau nol")
